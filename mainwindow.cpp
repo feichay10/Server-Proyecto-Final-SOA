e1ef6f5ff3b9e927fe9f691212723467c4cf06b9
javierD7 <alu0101432632@ull.edu.es>
2023-05-31 23:55:31 +0100

    Server receiving client connections correctly

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget* parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+  delete ui;
 }
 
+
+void MainWindow::on_actionServer_On_triggered() {
+  ///create socket, send some kind of response
+  server = new QTcpServer();
+
+  if (server->listen(QHostAddress::Any, 8080)) {
+    connect(server, SIGNAL(newConnection()), this, SLOT(manConn()));
+    QMessageBox::information(this, "Open", "Waiting connections...");
+
+  } else QMessageBox::information(this, "Listening error", "The server could not initialize the client connection listening process");
+}
+
+void MainWindow::manConn() {
+  while (server->hasPendingConnections()) {
+    QTcpSocket* new_socket = server->nextPendingConnection();
+    connection_list.push_front(new_socket);
+    findChild<QTextEdit*>("clients_msgsEdit")->insertPlainText("\nClient " + QString::number(new_socket->socketDescriptor()) + " connected...");
+    QMessageBox::information(this, "New client", ("\nClient " + QString::number(new_socket->socketDescriptor()) + " connected..."));
+    //connect(new_socket, SIGNAL(readyRead()), this, SLOT());
+  }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,33 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork/QTcpServer>
+#include <QtNetwork/QTcpSocket>
+#include <QByteArray>
+#include <QString>
+#include <QMessageBox>
+#include <forward_list>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+ private slots:
+  void on_actionServer_On_triggered();
+  void manConn();
+
+ private:
+  Ui::MainWindow* ui;
+  QTcpServer* server;
+  std::forward_list<QTcpSocket*> connection_list;
 };
 #endif // MAINWINDOW_H
