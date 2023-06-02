08b280c79e8b6c81d7d742f33eef2ea16a7f16d3
javierD7 <alu0101432632@ull.edu.es>
2023-06-02 20:53:41 +0100

    The server and the client exchange the necessary messages for sending an image from client to server; structure for receivement also prepared but not functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,11 +19,13 @@ MainWindow::~MainWindow() {
 void MainWindow::on_actionServer_On_triggered() {
   ///create socket, send some kind of response
   server = new QTcpServer();
+
   if (server->listen(QHostAddress::Any, 8080)) {
     connect(server, SIGNAL(newConnection()), this, SLOT(manageConnect()));
     ui->actionServer_On->setEnabled(false); ///< We turn off the On buttom
     ui->actionOff_Server->setEnabled(true); ///< We turn on the Off buttom
     QMessageBox::information(this, "Server is activated", "Waiting connections...");
+
   } else QMessageBox::critical(this, "Listening error", server->errorString());
 }
 
@@ -49,50 +51,61 @@ void MainWindow::manageConnect() {
 void MainWindow::clientInteraction() {
   QTcpSocket* client_conn = reinterpret_cast<QTcpSocket*>(sender());
   QByteArray message_from_client;
-  message_from_client = client_conn->readAll();
+
+  while (client_conn->bytesAvailable() > 0)
+    message_from_client = client_conn->readAll();
+
   if (message_from_client.toStdString() == "SEND_IMG") {
     client_conn->write("OK");
+    client_conn->flush();
     client_conn->waitForBytesWritten();
-    message_from_client = client_conn->readAll();
+    client_conn->waitForReadyRead();
+
+    while (client_conn->bytesAvailable() > 0)
+      message_from_client = client_conn->readAll();
+
+    QMessageBox::information(this, windowTitle(), "The client sent this: ");
     QImage image_from_client;
     bool delivery_success = image_from_client.loadFromData(message_from_client);
-    if (delivery_success) {
-        ui->lineEdit_client_number->setText("Client " + QString::number(client_conn->socketDescriptor()) + " sent this image:");
-        QPixmap pixmap_image_client = QPixmap::fromImage(image_from_client);
-        ui->label_to_show_image->resize(pixmap_image_client.width(), pixmap_image_client.height());
-        ui->label_to_show_image->setPixmap(pixmap_image_client);
-    } else {
-        QMessageBox::critical(this, "Error: Cannot be able to get the image", "The image from client " + QString::number(server->nextPendingConnection()->socketDescriptor()) + " has some problems to be read");
-    }
-  } else if (message_from_client.toStdString() == "RECEIVE_IMG") {
 
+    if (delivery_success) {
+      ui->lineEdit_client_number->setText("Client " + QString::number(client_conn->socketDescriptor()) + " sent this image:");
+      QPixmap pixmap_image_client = QPixmap::fromImage(image_from_client);
+      ui->label_to_show_image->resize(pixmap_image_client.width(), pixmap_image_client.height());
+      ui->label_to_show_image->setPixmap(pixmap_image_client);
+
+    } else
+      QMessageBox::critical(this, "Error: Cannot be able to get the image", "The image from client " + QString::number(server->nextPendingConnection()->socketDescriptor()) + " has some problems to be read");
+
+  } else if (message_from_client.toStdString() == "RECEIVE_IMG") {
   } else {
     QMessageBox::critical(this, "ERROR: Command to server is not recognized", "The client " + QString::number(client_conn->socketDescriptor()) + " wants to do an unknown action");
     client_conn->write("ERROR1");
     client_conn->waitForBytesWritten();
   }
+
   // client_conn->write("¿Pasó usted por casa?"); //temporarily
 }
 
-void MainWindow::on_actionOff_Server_triggered()
-{
+void MainWindow::on_actionOff_Server_triggered() {
   for(auto i : connection_list) {
     i->disconnect(); ///< We disconnect each socket before delete it
     delete i; ///< We delete each socket of server
   }
+
   connection_list.clear();
   delete server;
   server = NULL;
-  if (!connection_list.empty()) {
+
+  if (!connection_list.empty())
     QMessageBox::critical(this, "Error: The task to clean the client's list failed", "The server had some problems to shut down, try again");
-  }
+
   ui->actionServer_On->setEnabled(true); ///< We turn on the On buttom
   ui->actionOff_Server->setEnabled(false); ///< We turn  the Off buttom
   QMessageBox::information(this, "Shutdown successful", "The server was turn off successful");
 }
 
-void MainWindow::on_actionClose_Data_Base_triggered()
-{
+void MainWindow::on_actionClose_Data_Base_triggered() {
   ui->actionClose_Data_Base->setEnabled(false); ///< We turn on the close data base buttom
 }
 
